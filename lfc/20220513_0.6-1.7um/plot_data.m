5dcd5c9ca30e1b5d989edd84b5bd56c5bb1e58a8
abgibbs <abgibbs@email.arizona.edu>
2022-06-08 10:32:43 -0700

    Initial commit

---
+++ lfc/20220513_0.6-1.7um/plot_data.m
@@ -0,0 +1,50 @@
+close all
+clear
+clc
+%% plot spectrum trace under each attenuation
+figure('Position', [100 100 1000 500])
+hold on
+load(strcat( "disp2_2.145_disp3_-0.05_ptamp_4.5A_0.9-1.7um_highres.mat"))
+plot(OSAWavelength, OSAPower, 'DisplayName','broad band LFC', 'Linewidth', 1)
+
+
+ylim([-70 10])
+xlabel('Wavelength(nm)')
+ylabel('Power(dbm)')
+legend('location','best')
+set(gca,'tickdir','out');
+set(gca,'box','on');
+grid on
+ax2 = axes('Position',get(gca,'Position'),...
+           'XAxisLocation','top',...
+           'YAxisLocation','right',...
+           'Color','none',...
+           'XColor','k','YColor','k');
+set(ax2,'YTick', []);
+set(ax2,'XTick', []);
+title(strcat("Broader comb"))
+
+%% plot spectrum trace under each attenuation
+figure('Position', [100 100 1000 500])
+hold on
+load(strcat( "disp2_2.145_disp3_-0.05_ptamp_4.5A_0.9-1.7um_lowres.mat"))
+plot(OSAWavelength, OSAPower, 'DisplayName','broad band LFC', 'Linewidth', 1)
+
+
+ylim([-70 10])
+xlabel('Wavelength(nm)')
+ylabel('Power(dbm)')
+legend('location','best')
+set(gca,'tickdir','out');
+set(gca,'box','on');
+grid on
+ax2 = axes('Position',get(gca,'Position'),...
+           'XAxisLocation','top',...
+           'YAxisLocation','right',...
+           'Color','none',...
+           'XColor','k','YColor','k');
+set(ax2,'YTick', []);
+set(ax2,'XTick', []);
+title(strcat("Broader comb"))
+
+
